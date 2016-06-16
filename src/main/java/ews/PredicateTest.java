c112d85c1687162d1f8ca90efe4230a3974d689c
edubossa <ews-725007>
2016-06-16 18:51:13 -0300

    Refactory

--- src/main/java/ews/PredicateTest.java
+++ src/main/java/ews/PredicateTest.java
@@ -6,24 +6,27 @@ import java.util.function.Predicate;
 
 public class PredicateTest {
 
+    //Pode ser acessado por Method references
+    static void print(Integer n){
+        System.out.print(n + "|");
+    }
+
 	public static void main(String[] args) {
 
 		List<Integer> numeros = new ArrayList<>();
-
-		numeros.add(10);
-		numeros.add(20);
-		numeros.add(30);
-		numeros.add(40);
-		numeros.add(50);
-		numeros.add(60);
-		numeros.add(70);
-		numeros.add(80);
-		numeros.add(90);
-		numeros.add(100);
-		
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+        numeros.add(40);
+        numeros.add(50);
+        numeros.add(60);
+        numeros.add(70);
+        numeros.add(80);
+        numeros.add(90);
+        numeros.add(100);
 		List<Integer> numeros2 = new ArrayList<>(numeros);
 		
-		numeros.forEach(n -> System.out.print(n.intValue() + "|"));
+		numeros.forEach(PredicateTest::print); //print for Method references
 		System.out.println();
 		System.out.println("-------------------------------------------------");
 		
@@ -34,18 +37,19 @@ public class PredicateTest {
 				return (t < 50);
 			}
 		};
-		
-		
+
 		numeros.removeIf(filter);
 		
-		numeros.forEach(n -> System.out.print(n.intValue() + "|"));
+		numeros.forEach(PredicateTest::print);
 		System.out.println();
 		System.out.println("-------------------------------------------------");
 		
 		System.out.println("SIZE: " + numeros2.size());
 		System.out.println("-------------------------------------------------");
-		numeros2.removeIf(n -> n.intValue() < 50); // use lambda expression
-		numeros.forEach(n -> System.out.print(n.intValue() + "|"));
+        System.out.println("Removendo de forma simplificada com lambda");
+        System.out.println("-------------------------------------------------");
+        numeros2.removeIf(n -> n.intValue() < 50); // use lambda expression
+		numeros.forEach(PredicateTest::print);
 		System.out.println();
 		System.out.println("-------------------------------------------------");
 		
